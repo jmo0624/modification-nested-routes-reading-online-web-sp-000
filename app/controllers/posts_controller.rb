2a971373ecb3f278b91ad770403f992a4aff4927
Josh Morris <jmo0624@gmail.com>
2020-06-11 00:17:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,11 +26,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to post_path(@post)
+end
 
   def edit
     @post = Post.find(params[:id])
